0848ecd603f0125b600feb6bc8fbe1eb9efa1ed2
idknow94 <65952166+idknow94@users.noreply.github.com>
2025-10-19 18:18:27 +0300

    Added render support

--- KAP/KAP/settings.py
+++ KAP/KAP/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY = env('KEY')
 DEBUG = env.bool('DEBUG', default=True)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
-                 'kap-student-council.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://kap-student-council.up.railway.app']
+                 'kap-student-council.up.railway.app', 'kap-giau.onrender.com']
+CSRF_TRUSTED_ORIGINS = [
+    'https://kap-student-council.up.railway.app', 'https://kap-giau.onrender.com']
 
 # Application definition
 
