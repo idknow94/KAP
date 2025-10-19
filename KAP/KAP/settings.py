bc3f875d94ea6d48436076ca55d841273f02352d
idknow94 <65952166+idknow94@users.noreply.github.com>
2025-10-19 14:26:29 +0300

    collectstatic

--- KAP/KAP/settings.py
+++ KAP/KAP/settings.py
@@ -71,6 +71,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -160,9 +161,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
-# Optional: for WhiteNoise in production
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python KAP/manage.py collectstatic --noinput
 web: gunicorn KAP.wsgi:application --chdir KAP --bind 0.0.0.0:8000
